08065ad29d9897044905a0bf285a08dae407d3dd
yebali <yeseongkwon@htbeyond.com>
2022-09-20 21:40:05 +0900

    좌표계 변환 feign관련 기능 삭제

--- src/main/kotlin/com/yebali/dev/place/search/service/model/TransformCoordinatesResultModel.kt
+++
@@ -1,9 +0,0 @@
-package com.yebali.dev.place.search.service.model
-
-import com.yebali.dev.place.util.CoordinateSystem
-
-data class TransformCoordinatesResultModel(
-    val coordinateSystem: CoordinateSystem,
-    val x: Double,
-    val y: Double,
-)
--- src/main/kotlin/com/yebali/dev/place/util/CoordinateSystem.kt
+++
@@ -1,6 +0,0 @@
-package com.yebali.dev.place.util
-
-enum class CoordinateSystem(val value: String) {
-    WGS84("WGS84"),
-    KTM("KTM"),
-}
